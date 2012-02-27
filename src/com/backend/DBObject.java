5652c0e3d6c5c15897e5135d97cbf9b5478a80d1
samir42 <samir42@stanford.edu>
2012-02-26 20:51:35 -0800

    initial files for db and login... no implementation yet

---
+++ src/com/backend/DBObject.java
@@ -0,0 +1,16 @@
+package com.backend;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Author: Samir Patel
+
+public class DBObject {
+	
+	DBOject() {
+	}
+
+}
