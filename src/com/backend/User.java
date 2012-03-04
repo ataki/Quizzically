a671e7b02a15e83e044c451f62586997c522fed8
samir42 <samir42@stanford.edu>
2012-03-03 20:54:47 -0800

    expanded API

--- src/com/backend/User.java
+++ src/com/backend/User.java
@@ -3,6 +3,7 @@ package com.backend;
 import java.util.ArrayList;
 
 public class User {
+	
 	private int id;
 	private String name;
 	private String password;
@@ -14,18 +15,39 @@ public class User {
 		return 0;
 	}
 	
-	public boolean createUser() {
+	public boolean createUser(String user, String password) {
 		return false;
 	}
 	
 	public String getName(int id) {
-
 		return "";
 	}
 	
 	public ArrayList<String> getUsers(String filter) {
 		return null;
 	}
+//	DB implemenation in User.java  getUsers(String userFilter);
+/*
+	try {
+		stmt.executeQuery("SELECT * FROM "+ TABLE +" WHERE name LIKE \"%" + name +"%\"" );
+		ResultSet rs = stmt.getResultSet();
+		
+		ArrayList<User> usersList = new ArrayList<User>();
+		while(rs.next()){
+			User user = new User();
+			user.id = rs.getInt("id");
+			user.name = rs.getString("name");
+			user.password = rs.getString("password");
+			user.salt = rs.getInt("salt");
+			user.access = rs.getBoolean("access");
+			usersList.add(user);
+			return usersList;
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	*/
 	
 }
  
