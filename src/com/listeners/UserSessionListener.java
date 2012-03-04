1ac562c28b59a84169ba7307414b57579c7d4399
Yuan-Hung Yang <yangyh@stanford.edu>
2012-03-04 01:03:02 -0800

    Fixed the path

---
+++ src/com/listeners/UserSessionListener.java
@@ -0,0 +1,38 @@
+package com.listeners;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import com.models.User;
+
+/**
+ * Application Lifecycle Listener implementation class UserSessionListener
+ *
+ */
+@WebListener
+public class UserSessionListener implements HttpSessionListener {
+
+    /**
+     * Default constructor. 
+     */
+    public UserSessionListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see HttpSessionListener#sessionCreated(HttpSessionEvent)
+     */
+    public void sessionCreated(HttpSessionEvent event) {
+        User user = new User();
+        event.getSession().setAttribute("user", user);
+    }
+
+	/**
+     * @see HttpSessionListener#sessionDestroyed(HttpSessionEvent)
+     */
+    public void sessionDestroyed(HttpSessionEvent event) {
+        // TODO Auto-generated method stub
+    }
+	
+}
