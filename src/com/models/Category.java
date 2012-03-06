5594cc1870ef00633d7a312d5cf0de698544cc6b
Jim Zheng <jim32990@gmail.com>
2012-03-06 05:39:33 -0800

    With last commit

---
+++ src/com/models/Category.java
@@ -0,0 +1,10 @@
+package com.models;
+
+public class Category {
+	public String name;
+	public int quiz_id;
+	public Category(String name, int quiz_id) {
+		this.name = name;
+		this.quiz_id = quiz_id;
+	}
+}
